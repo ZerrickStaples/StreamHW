4a54718c9909b33a31e0ac70a6b1485f138ea2a4
ZerrickStaples <zerrick.staples@gmail.com>
2016-09-22 09:19:23 -0400

    Added files via upload.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an instance of StreamReader to read from a file.
+            StreamReader reader = new StreamReader("..\\..\\..\\Shakespere.txt");
+
+            
+                int lineNumber = 0;
+
+
+                //read the first line from the text file
+                string line = reader.ReadLine();
+
+                Console.WriteLine(line);
+
+
+            //read the rest of the file.
+            while (line != null)
+            {
+                lineNumber++;
+                Console.WriteLine("Line {0}: {1}", lineNumber, line);
+                line = reader.ReadLine();
+            }
+
+        }
+    }
+}
